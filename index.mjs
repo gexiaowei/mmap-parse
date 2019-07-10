17ad0fde8aecfc56e45412892e9685948b44d396
gexiaowei <gandxiaowei@gmail.com>
2019-07-11 00:28:47 +0800

    解释文件

--- index.mjs
+++ index.mjs
@@ -6,20 +6,62 @@
  */
 import unzipper from 'unzipper'
 import fs from 'fs-extra'
+import _ from 'lodash'
 import xml2js from 'xml2js'
 
 (async () => {
+  fs.ensureDir('./dist')
   await fs.createReadStream('source/data.mmap').pipe(unzipper.Extract({path: './.tmp'})).promise()
-  let data = fs.readFileSync('./.tmp/Document.xml')
-  let result = await parseString(data.toString())
-  console.log(result['Map']['OneTopic'][0]['Topic'][0]['Text'][0]['$']['PlainText'])
+  let source = fs.readFileSync('./.tmp/Document.xml')
+  let data = await parseString(source.toString())
+  let result = []
+  const root = data['Map']['OneTopic']['Topic']
+  flattenTree(root, result)
+  fs.writeJSONSync('./dist/source.json', result)
 })()
 
-const parseString = function (data) {
+function parseString (data) {
   return new Promise((resolve, reject) => {
-    xml2js.parseString(data, {tagNameProcessors: [xml2js.processors.stripPrefix]}, (error, result) => {
+    xml2js.parseString(data, {
+      mergeAttrs: true,
+      explicitArray: false,
+      tagNameProcessors: [xml2js.processors.stripPrefix]
+    }, (error, result) => {
       if (error) return reject(error)
       return resolve(result)
     })
   })
+}
+
+function flattenTree (data, result, parent) {
+  let tmp = {
+    id: data.OId,
+    topic: data.Text.PlainText,
+    expanded: false
+  }
+  if (data.NotesGroup) tmp.notes = data.NotesGroup.NotesXhtmlData.PreviewPlainText
+  if (data.AttachmentGroup) {
+    let {AttachmentData: attachment} = data.AttachmentGroup
+    attachment = Array.isArray(attachment) ? attachment : [attachment]
+    tmp.attachment = attachment.map(item => {
+      let id = item['Uri']['_'].replace('mmarch://bin/', '').replace('.bin', '')
+      fs.copySync(`./.tmp/bin/${id}.bin`, `./dist/${id}.png`)
+      return {
+        id,
+        name: item['FileName']
+      }
+    })
+  }
+  if (parent) tmp.parentid = parent
+  else tmp.isroot = true
+  result.push(tmp)
+  const parent_id = data.OId
+  if (data.SubTopics && data.SubTopics.Topic) {
+    if (!Array.isArray(data.SubTopics.Topic)) {
+      data.SubTopics.Topic = [data.SubTopics.Topic]
+    }
+    data.SubTopics.Topic.forEach(item => {
+      flattenTree(item, result, parent_id)
+    })
+  }
 }
